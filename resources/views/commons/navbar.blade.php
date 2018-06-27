685c6a8f7259b0f3bbb0a53c40fe7c865b37bb50
Makoto Hasegawa <ec2-user@ip-172-31-34-84.us-east-2.compute.internal>
2018-06-27 01:27:53 +0000

    AVE

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,19 +1,14 @@
-<header> <button type="button" >
-    <img src="{{secure_asset("images/latte.jpg") }}">
-</button>
-  
+<header>
+    <nav>
+        <div>
+          
           <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/">Home</a>
+             
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
+                    
                     @if (Auth::check())
                         
                         <li class="dropdown">
@@ -24,9 +19,7 @@
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                             </ul>
                         </li>
-                    @else
-                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                        <li>{!! link_to_route('login', 'Login') !!}</li>
+
                     @endif
                 </ul>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,71 @@
     @if (Auth::check())
         <?php $user = Auth::user(); ?>
     @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
-                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+                <style type="text/css">
+             
+             video {
+                position: fixed;
+                right: 0;
+                bottom: 0;
+                min-width: 100%;
+                min-height: 100%;
+                width: auto;
+                height: auto;
+                z-index: 1;
+            }
+
+            .text {
+                position: relative; /*必ず必要*/
+                z-index: 2; /*必ず必要*/
+                text-align:center;
+                width:50%;
+                                text-decoration: none;
+                margin: 0 auto;
+                margin-top: 690px;
+            
+            }
+            
+            .button{
+                display: inline-block;
+                text-decoration: none;
+                background: #FFFFFF;
+                color: #FFF;
+                width: 60px;
+                height: 60px;
+                line-height: 60px;
+                border-radius: 50%;
+                text-align: center;
+                font-weight: bold;
+                vertical-align: middle;
+                overflow: hidden;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
+                border-bottom: solid 3px #bd6565;
+                transition: .4s;
+                }
+                            
+            .button:hover{
+                  -webkit-transform: rotateY(360deg);
+                  -ms-transform: rotateY(360deg);
+                  transform: rotateY(360deg);
+                }
+                        
+            
+            </style>
+
+
+            <div class="text">
+                <span class="button" style="margin-right:200px;">{!! link_to_route('signup.get', 'S') !!}</span>
+                <span class="button">{!! link_to_route('login', 'L') !!}</span>
+              
             </div>
-        </div>
+            
+            <video src="siesta.mp4" autoplay controls ></video>
+            
+            
+       
+            
     @endif
+    
+    
+    
 @endsection
